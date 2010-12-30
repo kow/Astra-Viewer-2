8d434f39c34b7494e65d0bb1783104c648053b1f
Kirstenlee <Kirstenlee@aa1d9889-ed10-4cbe-9a23-cb7b022c8262>
2010-12-30 05:44:18 +0000

    removal of outdated S21 files
    git-svn-id: https://kirstensviewers.svn.sourceforge.net/svnroot/kirstensviewers/S21@559 aa1d9889-ed10-4cbe-9a23-cb7b022c8262

--- indra/cmake/GLOD.cmake
+++
@@ -1,9 +0,0 @@
-# -*- cmake -*-
-include(Prebuilt)
-
-if (NOT STANDALONE)
-  use_prebuilt_binary(GLOD)
-endif (NOT STANDALONE)
-
-set(GLOD_INCLUDE_DIR ${LIBS_PREBUILT_DIR}/include)
-set(GLOD_LIBRARIES glod)
--- indra/cmake/LLKDU.cmake
+++
@@ -1,7 +0,0 @@
-# -*- cmake -*-
-include(Prebuilt)
-
-if (INSTALL_PROPRIETARY AND NOT STANDALONE)
-  use_prebuilt_binary(kdu)
-  set(LLKDU_LIBRARY llkdu)
-endif (INSTALL_PROPRIETARY AND NOT STANDALONE)
