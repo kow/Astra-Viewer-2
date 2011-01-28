fb126efad2ff3f442fa08e28bce7828dc7bb0a21
Kirstenlee <Kirstenlee@aa1d9889-ed10-4cbe-9a23-cb7b022c8262>
2011-01-28 12:04:06 +0000

    mesh merge 27/01
    git-svn-id: https://kirstensviewers.svn.sourceforge.net/svnroot/kirstensviewers/S21@634 aa1d9889-ed10-4cbe-9a23-cb7b022c8262

--- indra/newview/llvoavatar.cpp
+++ indra/newview/llvoavatar.cpp
@@ -8172,12 +8172,16 @@ BOOL LLVOAvatar::updateLOD()
 		mNeedsSkin = TRUE;
 		mDrawable->clearState(LLDrawable::REBUILD_GEOMETRY);
 	}
-	
 	updateVisibility();
 
 	return res;
 }
 
+void LLVOAvatar::updateLODRiggedAttachments( void )
+{
+	updateLOD();
+	rebuildRiggedAttachments();
+}
 U32 LLVOAvatar::getPartitionType() const
 { 
 	// Avatars merely exist as drawables in the bridge partition
