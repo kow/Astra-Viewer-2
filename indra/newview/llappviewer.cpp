72741f9af9a167f955b3dab6d2aaf93cc2782562
kirstenlee <kirstenlee@aa1d9889-ed10-4cbe-9a23-cb7b022c8262>
2011-02-06 14:53:08 +0000

    Update Linden Skin
    git-svn-id: https://kirstensviewers.svn.sourceforge.net/svnroot/kirstensviewers/S21@668 aa1d9889-ed10-4cbe-9a23-cb7b022c8262

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -307,7 +307,7 @@ BOOL gLogoutInProgress = FALSE;
 
 ////////////////////////////////////////////////////////////
 // Internal globals... that should be removed.
-static std::string gArgs = "Build 4 Test";
+static std::string gArgs = "Build 4 RC1";
 
 const std::string MARKER_FILE_NAME("SecondLife.exec_marker");
 const std::string ERROR_MARKER_FILE_NAME("SecondLife.error_marker");
