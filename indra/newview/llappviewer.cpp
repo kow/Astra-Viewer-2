7ddc1cb86dd92e130d7231cfda4a709ac1ef3689
kirstenlee <kirstenlee@aa1d9889-ed10-4cbe-9a23-cb7b022c8262>
2011-02-23 09:01:01 +0000

    git-svn-id: https://kirstensviewers.svn.sourceforge.net/svnroot/kirstensviewers/S21@737 aa1d9889-ed10-4cbe-9a23-cb7b022c8262

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -307,7 +307,7 @@ BOOL gLogoutInProgress = FALSE;
 
 ////////////////////////////////////////////////////////////
 // Internal globals... that should be removed.
-static std::string gArgs = "Test 5.1";
+static std::string gArgs = "Test 5.2";
 
 const std::string MARKER_FILE_NAME("SecondLife.exec_marker");
 const std::string ERROR_MARKER_FILE_NAME("SecondLife.error_marker");
