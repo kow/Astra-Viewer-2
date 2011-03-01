7ff423771c5e5cd440fe0ec7cf745d07c2ecd16a
kirstenlee <kirstenlee@aa1d9889-ed10-4cbe-9a23-cb7b022c8262>
2011-03-01 09:38:39 +0000

    git-svn-id: https://kirstensviewers.svn.sourceforge.net/svnroot/kirstensviewers/S21@751 aa1d9889-ed10-4cbe-9a23-cb7b022c8262

--- indra/newview/app_settings/shaders/class3/effects/anaglyphV.glsl
+++ indra/newview/app_settings/shaders/class3/effects/anaglyphV.glsl
@@ -1,13 +1,18 @@
+#version 120
+
+varying vec2 vary_fragcoord;
+uniform vec2 screen_res;
+
 void main(void) 
 { 
         // Anaglyph Shader KL
 	
 	// Transform the pos to screen space
-	//gl_Position = ftransform();
+	gl_Position = ftransform();
 	gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+        //vary_fragcoord = (pos.xy*0.5+0.5)*screen_res;
 	
 	// use the first set of texture coord in the frag shader
-	gl_TexCoord[0] = gl_MultiTexCoord0;
-	
+	gl_TexCoord[0] = gl_MultiTexCoord0;	
 		
 }
